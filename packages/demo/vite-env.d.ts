4f037241281d2e6cfa85d35e93fcb8d66e1fee19
mav-rik <mav777art@gmail.com>
2026-04-20 12:26:35 +0200

    chore: scaffold packages/demo package shell

---
+++ packages/demo/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+declare interface AtscriptMetadata {}
+declare type AtscriptPrimitiveTags = string;
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent;
+  export default component;
+}
